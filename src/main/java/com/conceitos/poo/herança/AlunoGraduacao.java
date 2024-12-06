973a16cf7dd1160b489ca279ebbc04f93a756a69
Ícaro Alencar Sousa <ias_epf@hotmail.com>
2024-12-06 11:30:18 -0300

    Criada a classe AlunoGraduação que herda da classe aluno

--- src/main/java/com/conceitos/poo/herança/AlunoGraduacao.java
+++ src/main/java/com/conceitos/poo/herança/AlunoGraduacao.java
@@ -1,4 +1,25 @@
 package com.conceitos.poo.herança;
+import com.conceitos.poo.abstracao.Aluno;
 
-public class AlunoGraduacao {
+public class AlunoGraduacao extends Aluno {
+    private String curso;
+
+    public AlunoGraduacao(String nome, int idade, String matricula, String curso) {
+        super(nome, idade, matricula);
+        this.curso = curso;
+    }
+
+    public String getCurso() {
+        return curso;
+    }
+
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", curso=" + curso;
+    }
+
 }
